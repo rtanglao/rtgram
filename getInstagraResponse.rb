7c161b3cb7ef026b2234df43beffbec05918b3b0
Roland Tanglao <roland@rolandtanglao.com>
2014-09-11 18:53:56 -0700

    remove bogus file

--- getInstagraResponse.rb
+++
@@ -1,8 +0,0 @@
-require 'rubygems'
-require 'pp'
-require 'nestful'
-
-def getFlickrResponse(url, client_id, params)
-  url = "api.flickr.com/" + url
-  return Nestful.get url, :format => :json, :params => params
-end
