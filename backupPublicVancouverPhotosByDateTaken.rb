b4b90361bcc79611678f6481aab9b9ab8a5ec81a
Roland Tanglao <roland@rolandtanglao.com>
2014-09-14 23:21:38 -0700

    add another kludge: backup by max time by 24 hours when you detect the same max time from the previous api calls

--- backupPublicVancouverPhotosByDateTaken.rb
+++ backupPublicVancouverPhotosByDateTaken.rb
@@ -42,71 +42,77 @@ end
 url = 'media/search/'
 photosColl = db.collection("photos")
 MIN_DATE = Time.local(ARGV[0].to_i, ARGV[1].to_i, ARGV[2].to_i, 0, 0) # may want Time.utc if you don't want local time
-MAX_DATE = Time.local(ARGV[3].to_i, ARGV[4].to_i, ARGV[5].to_i, 23, 59) # may want Time.utc if you don't want local time
+MAX_DATE = Time.local(ARGV[3].to_i, ARGV[4].to_i, ARGV[5].to_i, 23, 59, 59) # may want Time.utc if you don't want local time
 
 min_taken_date  = MIN_DATE
-max_taken_date  = MIN_DATE + (60 * 60 * 24) - 1
-previous_max_date = Time.at(0)
-exit_this_date = false
-# 1st   6:59:50 aka 11:59:50p.m.
-# 100th 6:37:37 aka 11:37p.m.
-# Therefore take min time ie. the time of the 100th photo in this case and use that as max time
-$stderr.printf("min_taken:%s max_taken:%s\n", min_taken_date, max_taken_date)
-min_taken_date_from_instagram = MAX_DATE
-while max_taken_date > MIN_DATE && !exit_this_date
-  min_taken_date_str = min_taken_date.to_i.to_s
-  max_taken_date_str = max_taken_date.to_i.to_s
-  url_params = {
-    :client_id => client_id,
-    :lat => "49.260", # source: http://code.flickr.net/2008/09/04/whos-on-first/
-    :lng => "-123.113",
-    :distance => "5000",
-    :min_timestamp => min_taken_date_str,
-    :max_timestamp => max_taken_date_str,
-    :count => 100
-  }
-  vancouver_photos = getInstagramResponse(url, url_params)
-  if vancouver_photos["meta"]["code"] != 200
-    $stderr.printf("meta.code:%d\n", vancouver_photos["meta"]["code"])
-    break
-  end
-  if vancouver_photos.has_key?("pagination")
-    $stderr.printf("pagination: next_url:%s next_max_id:%s\n", 
-                   vancouver_photos["pagination"]["next_url"], 
-                   vancouver_photos["pagination"]["next_max_id"])
-  end
-  vancouver_photos["data"].each do|photo|
-    $stderr.printf("created_time:%s\n", photo["created_time"])
-    datetaken = Time.at(photo["created_time"].to_i)
-    datetaken = datetaken.utc
-    $stderr.printf("PHOTO datetaken:%s\n", datetaken)
-    photo["datetaken"] = datetaken
-    if datetaken < min_taken_date_from_instagram
-      min_taken_date_from_instagram = datetaken
+max_taken_date  = MAX_DATE - 1
+number_of_days_done = 0
+begin
+  previous_max_date = Time.at(0)
+  exit_this_date = false
+  # 1st   6:59:50 aka 11:59:50p.m.
+  # 100th 6:37:37 aka 11:37p.m.
+  # Therefore take min time ie. the time of the 100th photo in this case and use that as max time
+  $stderr.printf("min_taken:%s max_taken:%s\n", min_taken_date, max_taken_date)
+  min_taken_date_from_instagram = MAX_DATE
+  while max_taken_date > MIN_DATE && !exit_this_date
+    min_taken_date_str = min_taken_date.to_i.to_s
+    max_taken_date_str = max_taken_date.to_i.to_s
+    url_params = {
+      :client_id => client_id,
+      :lat => "49.260", # source: http://code.flickr.net/2008/09/04/whos-on-first/
+      :lng => "-123.113",
+      :distance => "5000",
+      :min_timestamp => min_taken_date_str,
+      :max_timestamp => max_taken_date_str,
+      :count => 100
+    }
+    vancouver_photos = getInstagramResponse(url, url_params)
+    if vancouver_photos["meta"]["code"] != 200
+      $stderr.printf("meta.code:%d\n", vancouver_photos["meta"]["code"])
+      break
     end
-    id = photo["id"]
-    existingPhoto =  photosColl.find_one("id" => id)
-    if existingPhoto
-      $stderr.printf("UPDATING photo id:%s\n",id)
-      photosColl.update({"id" =>id}, photo)
-    else
-      $stderr.printf("INSERTING photo id:%s\n",id)
-      photosColl.insert(photo)
+    if vancouver_photos.has_key?("pagination")
+      $stderr.printf("pagination: next_url:%s next_max_id:%s\n", 
+                     vancouver_photos["pagination"]["next_url"], 
+                     vancouver_photos["pagination"]["next_max_id"])
     end
-  end
+    vancouver_photos["data"].each do|photo|
+      $stderr.printf("created_time:%s\n", photo["created_time"])
+      datetaken = Time.at(photo["created_time"].to_i)
+      datetaken = datetaken.utc
+      $stderr.printf("PHOTO datetaken:%s\n", datetaken)
+      photo["datetaken"] = datetaken
+      if datetaken < min_taken_date_from_instagram
+        min_taken_date_from_instagram = datetaken
+      end
+      id = photo["id"]
+      existingPhoto =  photosColl.find_one("id" => id)
+      if existingPhoto
+        $stderr.printf("UPDATING photo id:%s\n",id)
+        photosColl.update({"id" =>id}, photo)
+      else
+        $stderr.printf("INSERTING photo id:%s\n",id)
+        photosColl.insert(photo)
+      end
+    end
 
-  # min_taken_date += (60 * 60 * 24)
-  max_taken_date = min_taken_date_from_instagram
-  $stderr.printf("END of API photo loop: max_taken_date:%s previous_max_date:%s\n",
-                 max_taken_date, previous_max_date)
-  if previous_max_date != Time.at(0)
-    if previous_max_date == max_taken_date
-      exit_this_date = true
-      $stderr.printf("setting exit_this_date to TRUE\n")
-    else
-      $stderr.printf("LEAVING exit_this_date set to FALSE\n")
+    # min_taken_date += (60 * 60 * 24)
+    max_taken_date = min_taken_date_from_instagram
+    $stderr.printf("END of API photo loop: max_taken_date:%s previous_max_date:%s\n",
+                   max_taken_date, previous_max_date)
+    if previous_max_date != Time.at(0)
+      if previous_max_date == max_taken_date
+        exit_this_date = true
+        # $stderr.printf("setting exit_this_date to TRUE\n")
+      else
+        # $stderr.printf("LEAVING exit_this_date set to FALSE\n")
+      end
     end
+    previous_max_date = max_taken_date    
   end
-  previous_max_date = max_taken_date    
-end
+  number_of_days_done += 1
+  max_taken_date = MAX_DATE - ((60 * 60 * 24) * number_of_days_done)
+  $stderr.printf("END of DAY photo loop: max_taken_date:%s, MIN_DATE:%s\n", max_taken_date, MIN_DATE)
+end while max_taken_date > MIN_DATE
 # pp vancouver_photos
