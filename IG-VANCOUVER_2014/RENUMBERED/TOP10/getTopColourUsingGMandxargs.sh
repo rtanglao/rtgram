2679ace2df83560ca398af14c0197c57f0775054
Roland Tanglao <roland@rolandtanglao.com>
2015-04-03 01:30:33 -0700

    add maxdepth to prevent recursion

--- IG-VANCOUVER_2014/RENUMBERED/TOP10/getTopColourUsingGMandxargs.sh
+++ IG-VANCOUVER_2014/RENUMBERED/TOP10/getTopColourUsingGMandxargs.sh
@@ -1,1 +1,1 @@
-find . -name '*.jpg' -print0 | xargs -0 -I file gm convert -crop 1x1+0+0 file TOPCOLOUR/file
+find . -maxdepth 1 -name ‘*.jpg’ -print0 | xargs -0 -I file gm convert -crop 1x1+0+0 file TOPCOLOUR/file
