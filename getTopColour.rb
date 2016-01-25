f8afae870acb651749162d5c59277f275fdc04e9
Roland Tanglao <roland@rolandtanglao.com>
2016-01-24 21:06:10 -0800

    add check for valid JPEG file

--- getTopColour.rb
+++ getTopColour.rb
@@ -1,13 +1,18 @@
 #!/usr/bin/env ruby
 require 'rubygems'
-require 'pp'
+require 'mini_magick'
 require 'miro'
 
+MiniMagick.configure do |config|
+  config.cli = :graphicsmagick
+end
+
 Dir.glob('*.jpg') do |jpg_file|
-  colors3 = Miro::DominantColors.new(jpg_file)
-  mimetype = `file -Ib #{jpg_file}`.gsub(/\n/,"")
-  if (mimetype <=> "image/jpeg; charset=binary") != 0
-    next
+  identify = MiniMagick::Tool::Identify.new
+  identify << jpg_file
+  id = identify.call
+  if id.include? "JPEG"
+    colors3 = Miro::DominantColors.new(jpg_file)
+    printf("%s\n", colors3.to_hex[0])
   end
-  printf("%s\n", colors3.to_hex[0])
 end
