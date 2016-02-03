bd9382c5756105c3a88014d8acfb494fd57e74f9
Roland Tanglao <roland@rolandtanglao.com>
2016-02-03 12:20:02 -0800

    working

--- getDominantColourInBuckets.rb
+++ getDominantColourInBuckets.rb
@@ -33,7 +33,8 @@ match = { '$match' =>
                                         "$lt" => next_bucket } }}
 red_green_blue_divide_by_100 =
   { '$project' =>
-    { "red" =>
+    { 
+      "red" =>
       { "$divide" => ["$top_colour.red", 255.0]},
       "green" =>
       { "$divide" => ["$top_colour.green", 255.0]},
@@ -43,7 +44,8 @@ red_green_blue_divide_by_100 =
   }
 red_green_blue_pow_22 =
   { '$project' =>
-    { "redpow" =>
+    {
+      "redpow" =>
       { "$pow" => ["$red", 2.2]},
       "greenpow" =>
       { "$pow" => ["$green", 2.2]},
@@ -51,17 +53,15 @@ red_green_blue_pow_22 =
       { "$pow" => ["$blue", 2.2]}
     }
   }
-slicetopcolour = { "$project" =>
-                   { "firstTopColour" =>
-                     { "$slice" => [ "$top5colours", 1 ] }}}
-sliceR = { "$project" =>
-           { "firstR" =>
-             { "$slice" => [ "$firstTopColour", 1 ] }}}
-sliceRR = { "$project" => { "firstRR" => { "$slice" => [ "$firstR", 1 ] }}}
-# project = {"$project" => {"id" => 1, "top5colours" => 1}}
-slice1 = { "$slice" =>["top5colours", 0,1]}
-u1 = {"$unwind" => "$top5colours"}
-#project = {"$project" => {"linear" => { "$divide" => [ "$top5colours[0][0]", 255.0 ]}}}
+red_green_blue_avg =
+  { '$group' =>
+    {
+      "_id" => nil,
+      "red_linear_avg" => { "$avg" => "$redpow" },
+      "green_linear_avg" => { "$avg" => "$greenpow" },
+      "blue_linear_avg" => { "$avg" => "$bluepow" }
+    }
+  }
 
 g1 = { "$group" => { "_id" => {"powlinear" => { "$pow" => [ "$linear", 2.2]}}}}
 # g1 = { '$group' =>
@@ -76,7 +76,8 @@ g1 = { "$group" => { "_id" => {"powlinear" => { "$pow" => [ "$linear", 2.2]}}}}
 #                            2.2 ]}}}}
 x = photosExtraMetadata.aggregate(
   [match, red_green_blue_divide_by_100,
-   red_green_blue_pow_22
+   red_green_blue_pow_22,
+   red_green_blue_avg
      # {
      #   "$group" =>
      #     {
