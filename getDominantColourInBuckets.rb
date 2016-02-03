caba1e0f015194b59739239b262fd73787aa45da
Roland Tanglao <roland@rolandtanglao.com>
2016-02-03 12:32:48 -0800

    got inverse pow  working

--- getDominantColourInBuckets.rb
+++ getDominantColourInBuckets.rb
@@ -53,7 +53,7 @@ red_green_blue_pow_22 =
       { "$pow" => ["$blue", 2.2]}
     }
   }
-red_green_blue_avg =
+red_green_blue_linear_avg =
   { '$group' =>
     {
       "_id" => nil,
@@ -62,22 +62,20 @@ red_green_blue_avg =
       "blue_linear_avg" => { "$avg" => "$bluepow" }
     }
   }
+red_green_blue_int_avg =
+  { '$project' =>
+    {
+      "red_int_avg" => { "$pow" => ["$red_linear_avg", 1.0/2.2] },
+      "green_int_avg" => { "$pow" => ["$green_linear_avg", 1.0/2.2] },
+      "blue_int_avg" => { "$pow" => ["$green_linear_avg", 1.0/2.2] }
+    }
+  }
 
-g1 = { "$group" => { "_id" => {"powlinear" => { "$pow" => [ "$linear", 2.2]}}}}
-# g1 = { '$group' =>
-#        {"_id" => "id",
-#         "linear" => {"$divide" => ["$top5colours[0][0]",
-#                                                        255.0]}}}
-# group = { '$group' =>
-#           {"_id" => "id",
-#            { "$avg" => { "$pow" => [
-#                            {"$divide" => ["top5colours[0][0]",
-#                                                        255.0]},
-#                            2.2 ]}}}}
 x = photosExtraMetadata.aggregate(
   [match, red_green_blue_divide_by_100,
    red_green_blue_pow_22,
-   red_green_blue_avg
+   red_green_blue_linear_avg,
+   red_green_blue_int_avg
      # {
      #   "$group" =>
      #     {
