9f9f0374fab066ddc413c979b74aca4e4d1fc3c9
Roland Tanglao <roland@rolandtanglao.com>
2016-02-07 11:14:16 -0800

    WIP need to get lat/lon and dump to csv

---
+++ writeCSVTopColour.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'json'
+require 'pp'
+require 'mongo'
+
+MONGO_HOST = ENV["MONGO_HOST"]
+raise(StandardError,"Set Mongo hostname in ENV: 'MONGO_HOST'") if !MONGO_HOST
+MONGO_PORT = ENV["MONGO_PORT"]
+raise(StandardError,"Set Mongo port in ENV: 'MONGO_PORT'") if !MONGO_PORT
+MONGO_USER = ENV["MONGO_USER"]
+MONGO_PASSWORD = ENV["MONGO_PASSWORD"]
+INSTAGRAM_DB = ENV["INSTAGRAM_DB"]
+raise(StandardError,"Set Mongo instagram database name in ENV: 'INSTAGRAM_DB'") if !INSTAGRAM_DB
+
+db = Mongo::Client.new([MONGO_HOST], :database => INSTAGRAM_DB, :port => MONGO_PORT)
+if MONGO_USER
+  auth = db.authenticate(MONGO_USER, MONGO_PASSWORD)
+  if !auth
+    raise(StandardError, "Couldn't authenticate, exiting")
+    exit
+  end
+end
+Mongo::Logger.logger.level = ::Logger::FATAL # http://stackoverflow.com/questions/30292100/how-can-i-disable-mongodb-log-messages-in-console
+
+photosExtraMetadata = db[:photosExtraMetadata]
+photosColl = db[:photos]
+
+photosExtraMetadata.find({"valid150x150jpg" => true }).sort({"datetaken" => 1}).each do |photo|
+  pp photo
+  # top_colour = { "red" => photo["top5colours"][0][0],
+  #                "blue" => photo["top5colours"][0][1],
+  #                "green" => photo["top5colours"][0][2]}
+  # extraMetadata = photosExtraMetadata.update_one(
+  #   { "id" => photo["id"]},
+  #   { "$set" => { "top_colour" => top_colour}})
+  # pp extraMetadata
+end
