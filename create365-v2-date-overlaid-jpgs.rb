628897471c7326a977d1802f21b1413254cc6c67
Roland Tanglao <roland@rolandtanglao.com>
2016-03-08 21:49:05 -0800

    add 2nd version of create date overlaid jpgs

---
+++ create365-v2-date-overlaid-jpgs.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'open3'
+
+date = Time.local(2015, 1, 1, 0, 0) # may want Time.utc if you don't want local time
+day_number = 0
+Dir.glob('/Users/rtanglao/Dropbox/GIT/rtgram/2015-IG-VAN-365-ENF-DATAMAPS-P50-PNGS/MOGRIFY_JPEGS/1920/*-ig-vancouver-topcolour.jpg') do | jpg_file |
+  day_number += 1
+  puts jpg_file
+  time_str = date.strftime("%A_%b_%-d_%Y")
+  new_file_name = sprintf("%3.3d-%s", day_number, time_str) + ".jpg"
+  puts new_file_name
+  puts time_str
+  stdout, stderr, status = Open3.capture3(
+                    "convert -verbose -font Times-Bold -pointsize 32 " +
+                    jpg_file +
+                    " -fill white  -undercolor '#00000080' " +
+                    "-gravity southeast -annotate +0+5 '" +
+                    time_str + "'" +
+                    " " +
+                    "ANNOTATED_WITH_DATE/" + new_file_name)
+  print stderr      
+  date += 60 * 60 * 24
+end
